4821ecbe1936f1e4b4ff91bbdffbff9796b032d7
Robert Gaffney <rgaffney@mailcan.com>
2019-08-31 02:16:21 -0500

    fun bg effects

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import * as THREE from './node_modules/three';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.z = 10;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -19,6 +20,27 @@ var cube2 = new THREE.Mesh( geometry2, material2 );
 cube2.position.set(-4, 0, 0);
 scene.add( cube2 );
 
+function quickRand(): number {
+	return (Math.random() - Math.random()) * 20;
+}
+
+var cubeList: THREE.Mesh[] = [];
+
+function addNewRandomCube() {
+	var geometryTemp = new THREE.BoxGeometry( 1,1,1 );
+    var materialTemp = new THREE.MeshStandardMaterial( { color: 0x8888ff } );
+    var cubeTemp = new THREE.Mesh( geometryTemp, materialTemp );
+    cubeTemp.position.set(quickRand(), quickRand(), Math.random() * .5 - 10);
+    cubeTemp.name="hehe" + Math.random(); 
+    scene.add( cubeTemp );
+    cubeList.push(cubeTemp);
+    console.log("cubeList.length" + cubeList.length);
+}
+
+for (var i = 0; i < 300; i++) {
+    addNewRandomCube();
+}
+
 var light = new THREE.PointLight( 0x00ffff, 1, 100 );
 light.position.set( 5, 5, 5 );
 scene.add( light );
@@ -35,7 +57,7 @@ var light4 = new THREE.PointLight( 0xffffff, 1, 100 );
 light4.position.set( 2, 10, -2 );
 scene.add( light4 );
 
-camera.position.z = 5;
+var counter = 0;
 
 var animate = function () {
   requestAnimationFrame( animate );
@@ -49,7 +71,37 @@ var animate = function () {
   cube2.rotation.z += 0.02;
     
   //cube.position.x -= 0.01;
-   
+
+  if (counter > 10) {
+
+    var ind = Math.floor(Math.random() * cubeList.length);
+    if (scene.getObjectByName(cubeList[ind].name)) {
+       var obj : THREE.Object3D = scene.getObjectByName(cubeList[ind].name) as THREE.Object3D;
+       scene.remove( obj as THREE.Object3D);
+       cubeList.splice(ind, 1);
+       addNewRandomCube();
+    }
+    counter = 0;
+  }
+
+  //var ind = Math.floor(Math.random() * cubeList.length);
+  //if (scene.getObjectByName(cubeList[ind].name)) {
+  //   obj = scene.getObjectByName(cubeList[ind].name) as THREE.Object3D;
+     //var x: number = Math.random();
+     //if (x < 0.33) {
+     //    obj.rotation.x += 0.1;
+     //} else if (x > 0.66) {
+     //    obj.rotation.y += 0.1;
+     //} else {
+     //    obj.rotation.z += 0.1;
+     //}
+     //obj.rotation.x += 0.5;
+  //}
+
+
+
+  counter++;
+
   renderer.render( scene, camera );
 };
 
