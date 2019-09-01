9922700eb5db7a7af02efcc4f9c4bcd9689847af
Robert Gaffney <rgaffney@mailcan.com>
2019-09-01 14:42:58 -0500

    separate src and dist

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three';
+import * as THREE from '../node_modules/three';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
